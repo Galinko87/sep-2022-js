79d5d931a3a165df6be48cd391c7e2926ce638bf
GalinaPrikhodchenko <galina.prikhodchenko@gmail.com>
2022-11-01 16:59:30 +0100

    task1 lesson 1 js

---
+++ lesson1/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let greet = "hello";
+console.log(greet);
+let address = "owu";
+console.log(address);
+let domen = "com";
+console.log(domen);
+let domen2 = "ua";
+console.log(domen2);
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+const PI = 3.14;
+console.log(PI);
+let num4 = 2.7;
+console.log(num4);
+let num5 = 16;
+console.log(num5);
+let d = true;
+console.log(d);
+let e = false;
+console.log(e);
+
+let firstName = "galyna";
+let middleName = "volodymyrivna";
+let lastName = "prykhodchenko";
+let person = `${firstName} ${middleName} ${lastName}`;
+console.log(person);
+let a = 100;
+console.log(typeof a);
+let b = "100";
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+let input1 = prompt("what is your name?");
+console.log(input1);
+let input2 = prompt("what is your middle name?");
+console.log(input2);
+let input3 = prompt("what is your age?");
+console.log(input3);
+
+
+
+
+
+
+
+
